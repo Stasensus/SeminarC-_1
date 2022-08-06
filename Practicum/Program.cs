5585dca8daeae93c9278a0e70578101febf0ab43
Stasensus <stanislav@kireev.ru>
2022-08-06 17:03:58 +0300

    Добавили программы семинара

---
+++ Practicum/Program.cs
@@ -0,0 +1,38 @@
+﻿// int number = Convert.ToInt32(Console.ReadLine());
+
+// int square = number * number;
+
+// double testResult = Math.Pow(number, 3);
+
+// Console.WriteLine(square);
+
+// Console.WriteLine("Результат: " + testResult);
+
+
+
+// System.Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// double square = Math.Pow(num2, 2);
+// if (square == num1)
+//     {
+//         System.Console.WriteLine("Является");
+//     }
+//     else System.Console.WriteLine("Не является");
+
+
+
+// System.Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0)
+// {
+//     number = number * (-1);
+// }
+// int negativeNumber = number * (-1);
+// while (negativeNumber <= number)
+// {
+//     System.Console.WriteLine(negativeNumber);
+//     negativeNumber ++;    
+// }
+// System.Console.WriteLine(number);
