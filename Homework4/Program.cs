b5ddcf64b1b5c746c07cf0428090ad4a1f075ccf
Stasensus <stanislav@kireev.ru>
2022-08-06 17:47:36 +0300

    Решили четвертую задачу

---
+++ Homework4/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = 2;
+if (number > 2)
+{
+    // number1 = 2;
+    while (number1 <= number)
+    {
+        System.Console.WriteLine(number1);
+        number1 = number1 + 2;
+    }
+}
+if (number < 2)
+{
+    System.Console.WriteLine("Некорректная цифра. Чётных нет");
+}
+if (number == 2)
+{
+    System.Console.WriteLine(number);
+}
