638c03fa79a03ec5775cada51f398f12a80295c5
Stasensus <stanislav@kireev.ru>
2022-08-06 17:04:21 +0300

    Добавили первую программу ДЗ

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    System.Console.WriteLine("Первое число больше второго");
+}
+else System.Console.WriteLine("Второе число больше первого");
