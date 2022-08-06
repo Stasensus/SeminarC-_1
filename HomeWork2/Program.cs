40616e6928f1a9346b4ea7a88132f72b4835870a
Stasensus <stanislav@kireev.ru>
2022-08-06 17:13:32 +0300

    Решили вторую задачу

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (num2 > num1)
+{
+    max = num2;
+}
+else max = num1;
+if (num3 > max)
+{
+    System.Console.WriteLine("Максимальное число " + num3);
+}
+else System.Console.WriteLine("Максимальное число " + max);
